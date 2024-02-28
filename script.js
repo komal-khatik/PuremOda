616e258035f268ac14d976778f3291211f9a7512
komal-khatik <komalkhatik77@gmail.com>
2024-02-28 01:17:48 -0800

    landing page

---
+++ script.js
@@ -0,0 +1,53 @@
+var img = document.querySelector('#left-bot1');
+var img1 = document.querySelector('#left-bot2');
+
+img.addEventListener('mouseenter', function () {
+  img.innerHTML = '#rise';
+  img.style.color = 'White';
+  img.style.paddingTop = '170px';
+  img.style.paddingLeft = '25px';
+  img.style.fontSize = '16px';
+  img.style.fontWeight = '600';
+  img.style.textTransform = 'Uppercase';
+  img.style.letterSpacing = '.1rem';
+  img.style.backgroundImage =
+    'url(https://images.unsplash.com/photo-1505318985551-5793f4a707bd?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)';
+});
+
+img.addEventListener('mouseleave', function () {
+  img.innerHTML = '#rip stop';
+  img.style.color = 'White';
+  img.style.paddingTop = '170px';
+  img.style.paddingLeft = '25px';
+  img.style.fontSize = '16px';
+  img.style.fontWeight = '600';
+  img.style.textTransform = 'Uppercase';
+  img.style.letterSpacing = '.1rem';
+  img.style.backgroundImage =
+    'url(https://images.unsplash.com/photo-1516195851888-6f1a981a862e?q=80&w=1410&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)';
+});
+
+img1.addEventListener('mouseenter', function () {
+  img1.innerHTML = '#life';
+  img1.style.color = 'White';
+  img1.style.paddingTop = '170px';
+  img1.style.paddingLeft = '25px';
+  img1.style.fontSize = '16px';
+  img1.style.fontWeight = '600';
+  img1.style.textTransform = 'Uppercase';
+  img1.style.letterSpacing = '.1rem';
+  img1.style.backgroundImage =
+    'url(https://images.unsplash.com/photo-1541439998155-388623962c26?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)';
+});
+img1.addEventListener('mouseleave', function () {
+  img1.innerHTML = '#insulted';
+  img1.style.color = 'White';
+  img1.style.paddingTop = '170px';
+  img1.style.paddingLeft = '25px';
+  img1.style.fontSize = '16px';
+  img1.style.fontWeight = '600';
+  img1.style.textTransform = 'Uppercase';
+  img1.style.letterSpacing = '.1rem';
+  img1.style.backgroundImage =
+    'url(https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)';
+});
